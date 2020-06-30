8e4324d460718a2e8ec33df616cdd5d706929f6a
DESKTOP-D3DJ5AT\lenovo <azib.donia96@gmail.com>
2020-06-30 13:00:43 +0200

    commit

--- app/src/main/java/com/example/parcours_mobile_8_scanqrcode_broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/parcours_mobile_8_scanqrcode_broadcastreceiver/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.parcours_mobile_8_scanqrcode_broadcastreceiver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "email "+sharedpreferences.getString("user_email",null), Toast.LENGTH_SHORT).show();
 
 
+    }
+
+
+
+
+//    menu 1
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+//    menu 2
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout_btn)
+        {
+//            is_connected => false
+//            => loginActivity
+
+            sharedpreferences.edit().clear().apply();
+            sharedpreferences.edit().putBoolean("is_connected",false).apply();
+
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
